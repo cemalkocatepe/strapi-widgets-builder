1e0058b3b0a1af9081a493c128973e34f2dc6e82
Cemal <cemalkocatepe@gmail.com>
2025-04-29 13:55:36 +0300

    vite building conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"), // tüm component'ları dışa aktaran dosya
+      name: "StrapiWidgetsBuilder",
+      fileName: (format) => `main.${format}.js`,
+      formats: ["es", "cjs", "umd"], // isteğe göre
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // dış bağımlılıklar
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
