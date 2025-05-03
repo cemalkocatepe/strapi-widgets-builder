af201fb2b05b21e4a2e5101af4d40888f9b31226
Cemal <cemalkocatepe@gmail.com>
2025-05-03 20:57:35 +0300

    motion frame

--- src/context/widget-list/widget-list.component.jsx
+++ src/context/widget-list/widget-list.component.jsx
@@ -1,48 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Reorder } from "motion/react";
 // Components
 import { Box, Button, Flex, IconButton, IconButtonGroup } from "@strapi/design-system";
 import { Trash, Pencil, CheckCircle } from "@strapi/icons";
+import { WidgetItem } from "./widget-list.styles";
 // Store
 import usePageStore from "../../store/usePageStore";
 
 const WidgetListComponent = ({ widgetsData }) => {
   const setActivePage = usePageStore((state) => state.setActivePage);
+  const [valueWidgetsData, setValueWidgetsData] = useState(widgetsData);
+
+  useEffect(() => {
+    console.log(valueWidgetsData);
+  }, [valueWidgetsData]);
 
   return (
     <>
-      {widgetsData.map((widget) => {
-        return (
-          <Box
-            key={widget.id}
-            style={{
-              padding: "10px",
-              margin: "10px",
-              backgroundColor: "#fff",
-              borderRadius: "8px",
-              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-            }}>
-            <Flex alignItems="center" justifyContent="space-between">
-              <Box>
-                <h3>{widget.name}</h3>
-              </Box>
-              <Box>
-                <IconButtonGroup>
-                  <IconButton
-                    onClick={() => alert("Düzenle tıklandı! ID:" + widget.id)} // Düzenle butonuna tıklandığında yapılacak işlemler
-                    label="Düzenle"
-                    icon={<Pencil />}
-                  />
-                  <IconButton
-                    onClick={() => alert("Sil tıklandı! ID:" + widget.id)} // Düzenle butonuna tıklandığında yapılacak işlemler
-                    label="Sil"
-                    icon={<Trash />}
-                  />
-                </IconButtonGroup>
-              </Box>
-            </Flex>
-          </Box>
-        );
-      })}
+      <Reorder.Group as="div" axis="y" values={valueWidgetsData} onReorder={setValueWidgetsData}>
+        {valueWidgetsData.map((widget) => {
+          return (
+            <Reorder.Item as="div" key={widget.id} value={widget} whileDrag={{ opacity: 0.4 }}>
+              <WidgetItem>
+                <Flex alignItems="center" justifyContent="space-between">
+                  <Box>
+                    <h3>{widget.name}</h3>
+                  </Box>
+                  <Box>
+                    <IconButtonGroup>
+                      <IconButton
+                        onClick={() => alert("Düzenle tıklandı! ID:" + widget.id)} // Düzenle butonuna tıklandığında yapılacak işlemler
+                        label="Düzenle"
+                        icon={<Pencil />}
+                      />
+                      <IconButton
+                        onClick={() => alert("Sil tıklandı! ID:" + widget.id)} // Düzenle butonuna tıklandığında yapılacak işlemler
+                        label="Sil"
+                        icon={<Trash />}
+                      />
+                    </IconButtonGroup>
+                  </Box>
+                </Flex>
+              </WidgetItem>
+            </Reorder.Item>
+          );
+        })}
+      </Reorder.Group>
       <Box display="flex" style={{ justifyContent: "center" }}>
         <Button variant="default" size="M" startIcon={<CheckCircle />} onClick={() => setActivePage("widget-create")}>
           Yeni Bölüm
